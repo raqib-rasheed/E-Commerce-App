1b78b739a8c2b43050639b751f068c8722609021
Raqib Rasheed <raqib.abdulrasheed@outlook.com>
2020-11-02 18:33:39 +0530

    database created

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const usersRepo = require('./repositories/user');
 
 const app = express();
-//here app is a massive object...,expecting a desteructring within {app}
 
-
+app.use(bodyParser.urlencoded({extended:true}));
+//here app is a massive object...,
 app.get('/',(req,res) =>{
     res.send(`
     <div>
-        <form>
-            <input placeholder="email">
-            <input placeholder="email">
-            <input placeholder="email">
+        <form method="POST">
+            <input name="name" placeholder="email">
+            <input name="password" placeholder="password">
+            <input name="passwordConfirmation" placeholder="password confirm">
             <button>Sign Up</button>
         </form>
     </div>
     `);
 });
 
-app.listen(3000,()=>{
+app.post("/",async (req,res) => {
+//get access to email,password,password Confirmation.
+//req.body is an object contaning the entered pass ema.... by the user
+const { email, password,passwordConfirmation} = req.body;
+const existingUser = await usersRepo.getOneBy({ email });
+    if(existingUser){
+    return res.send('Email already in use');
+}
+    if(password!==passwordConfirmation){
+        return res.send('passwords must match');
+    }
+    res.send('account created');
+});
+
+app.listen(3001,()=>{
     console.log("listening");
 });
